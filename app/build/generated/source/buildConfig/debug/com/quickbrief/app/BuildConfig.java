837f7e660fea7c32e478f4d5e79e547601f0655c
Nisar Ahmed <nisarahmed7988@gmail.com>
2025-06-21 19:57:23 +0530

    update branch

--- app/build/generated/source/buildConfig/debug/com/quickbrief/app/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/quickbrief/app/BuildConfig.java
@@ -9,4 +9,6 @@ public final class BuildConfig {
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
+  // Field from build type: debug
+  public static final String NEWS_API_KEY = "pub_3f8ab0e9e2f94719a0d40f0a3142e17b";
 }
